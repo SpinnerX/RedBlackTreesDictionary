f044488283271b0deeb14151c325c9192cfd0f67
schmij03 <schmij03@students.zhaw.ch>
2022-12-01 15:52:44 -0800

    new main

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
-public class Main{
-	public static void main(String[] args){
+public class Main {
+	public static void main(String[] args) {
 		System.out.println("Testing main java file.");
+
+		Loading l = new Loading();
+		ArrayList<String> d = l.LoadDictionary();
+		System.out.println(l.contains(d, "aback"));
 	}
+
+	
 }
