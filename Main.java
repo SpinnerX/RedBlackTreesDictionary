5a4af3390ab06f37c58a009a6692ba9428dd7295
SpinnerX <heraaronhotmail@yahoo.com>
2022-12-03 16:02:01 -0800

    Main src file testing RedBlackTrees

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
-import java.util.*;
+import RedBlackTrees.RedBlackTrees;
 
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Testing main java file.");
+		RedBlackTrees rbt = new RedBlackTrees();
+
+		rbt.add("Taurus");
+		rbt.add("Carl");
+		rbt.add("Adam");
+		rbt.add("Al");
+
+		rbt.print();
+
+		System.out.println(rbt.size());
+
+		rbt.remove("Carl");
+
+		rbt.print();
+		System.out.println(rbt.size());
 	}
 }
