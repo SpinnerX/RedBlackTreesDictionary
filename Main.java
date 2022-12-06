b9eef5c0e05c17924710b073fee6cf5b9dc8364e
SpinnerX <heraaronhotmail@yahoo.com>
2022-12-06 15:41:06 -0800

    Updated Main

--- Main.java
+++ Main.java
@@ -1,34 +1,61 @@
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import RedBlackTrees.RedBlackTrees;
 
+
 public class Main{
+
+	public static void LoadDictionary(RedBlackTrees rbt) {
+        ArrayList<String> dict = new ArrayList<String>();
+        try {
+            URL dic = new URL("http://www.math.sjsu.edu/~foster/dictionary.txt");
+            Scanner s = new Scanner(dic.openStream());
+            dict.add(s.next());//adds the first line
+            while (s.hasNextLine()) {
+                // dict.add(s.nextLine());//adds all the rest of the lines
+				String str = s.nextLine().trim();
+
+				String upperStr = str.toUpperCase();
+				rbt.add(upperStr);
+            }
+            s.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+	public static void Verify(RedBlackTrees rbt) {
+        ArrayList<String> dict = new ArrayList<String>();
+		int count = 0;
+        try {
+			String urlName = "http://www.math.sjsu.edu/~foster/dictionary.txt";
+            URL dic = new URL(urlName);
+            Scanner s = new Scanner(dic.openStream());
+            dict.add(s.next());//adds the first line
+			
+            while (s.hasNextLine()) {
+				String str = s.nextLine().trim();
+
+				String upperStr = str.toUpperCase();
+				if(!rbt.contains(upperStr)) count++;
+            }
+            s.close();
+        }
+		catch (IOException ex) { ex.printStackTrace(); }
+
+		if(count == 0) System.out.println("All Tests Passed!");
+		else System.out.println("Total Count: " + count);
+    }
+
 	public static void main(String[] args){
-		System.out.println("Testing main java file.");
-
-		Loading l = new Loading();
-		ArrayList<String> d = l.LoadDictionary();
-		System.out.println(l.contains(d, "aback"));
-		ArrayList<String> b = l.LoadPoem();
-		System.out.println(l.contains(d, b.get(4)));
-		//l.printPoem();
-
 		RedBlackTrees rbt = new RedBlackTrees();
 
-		rbt.add("Taurus");
-		rbt.add("Carl");
-		rbt.add("Adam");
-		rbt.add("Al");
+		LoadDictionary(rbt);
 
-		rbt.print();
+		Verify(rbt);
 
-		System.out.println(rbt.size());
-
-		rbt.remove("Carl");
-
-		rbt.print();
-		System.out.println(rbt.size());
 	}
-
-	
 }
