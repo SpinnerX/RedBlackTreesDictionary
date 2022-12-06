06e52b1a2510c97398827a45b24f6ae5a07e01f0
schmij03 <schmij03@students.zhaw.ch>
2022-12-05 21:18:12 -0800

    updated so that the first line will also get added

--- Loading.java
+++ Loading.java
@@ -3,7 +3,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import RedBlackTrees.RedBlackTrees;
+
 public class Loading {
+    RedBlackTrees rb = new RedBlackTrees();
+
     public boolean contains(ArrayList<String> s, String b) {
 
         if (s.contains(b)) {
@@ -18,9 +22,9 @@ public class Loading {
         try {
             URL dic = new URL("http://www.math.sjsu.edu/~foster/dictionary.txt");
             Scanner s = new Scanner(dic.openStream());
-
+            dict.add(s.next());//adds the first line
             while (s.hasNextLine()) {
-                dict.add(s.nextLine());
+                dict.add(s.nextLine());//adds all the rest of the lines
             }
             s.close();
         } catch (IOException ex) {
@@ -36,6 +40,7 @@ public class Loading {
             URL dic = new URL("https://raw.githubusercontent.com/schmij03/RedBlackTreesDictionary/main/poem.txt");
             Scanner s = new Scanner(dic.openStream());
 
+            poem.add(s.next());//ADDS first line
             while (s.hasNextLine()) {
                 poem.add(s.nextLine());
             }
@@ -47,14 +52,14 @@ public class Loading {
         return poem;
     }
 
-    public void printPoem(){
-        ArrayList<String>c = LoadPoem();
+    public void printPoem() {
+        ArrayList<String> c = LoadPoem();
 
-        for (int i=0; i<c.size();i++){
+        for (int i = 0; i < c.size(); i++) {
 
             System.out.println(c.get(i));
         }
 
-
     }
+
 }
