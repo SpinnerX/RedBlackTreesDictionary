e32cccf6b27caa57a1bf4bb278678f51129665d8
SpinnerX <heraaronhotmail@yahoo.com>
2022-12-06 22:53:42 -0800

    Checking for poems if it has been read and contains.

--- Tests/Test2.java
+++ Tests/Test2.java
@@ -2,51 +2,42 @@ package Tests;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import RedBlackTrees.RedBlackTrees;
+import RedBlackTrees.Loading;
+
 
 public class Test2 {
-
     RedBlackTrees rbt;
+    Loading load;
 
     @Before
     public void setup(){
         rbt = new RedBlackTrees();
+        
+        // Adding into rbt.
+
+        // Load(rbt);
+        // String filename = "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt";
+        // "https://raw.githubusercontent.com/schmij03/RedBlackTreesDictionary/main/poem.txt
+        String filename = "https://raw.githubusercontent.com/schmij03/RedBlackTreesDictionary/main/poem.txt";
+        load.LoadPoem(rbt, filename);
     }
 
     @Test
-    public void timeRedBlackTrees2(){
-        ArrayList<String> dict = new ArrayList<String>();
-		int count = 0;
-        long start=0, end=0;
-        try {
-			String urlName = "http://www.math.sjsu.edu/~foster/dictionary.txt";
-            URL dic = new URL(urlName);
-            Scanner s = new Scanner(dic.openStream());
-            dict.add(s.next());//adds the first line
-            start = System.currentTimeMillis();
+    public void testContains(){
+        int count = load.Verify(rbt);
 
-            while (s.hasNextLine()) {
-				String str = s.nextLine().trim();
-
-				String upperStr = str.toUpperCase();
-				if(!rbt.contains(upperStr)) count++;
-            }
-
-            end = System.currentTimeMillis();
-            s.close();
-        }
-		catch (IOException ex) { ex.printStackTrace(); }
-
-		if(count == 0) System.out.println("All Tests Passed!");
-		else System.out.println("Total Count: " + count);
-        
-        float current = end - start;
-        System.out.println("Current Time: " + current/10);
+        assertEquals(0, count);
     }
-    
+
+
+
 }
